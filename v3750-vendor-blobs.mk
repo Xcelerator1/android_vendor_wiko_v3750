2bd27b1fa2f10498b3faaee5530ecf23d9c5a9a4
Ahmed Rahmy <ahmedrahmy842@gmail.com>
2017-06-11 20:37:00 +0200

    Added mk files

---
+++ v3750-vendor-blobs.mk
@@ -0,0 +1,11 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/v3750/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/v3750/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/v3750/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/v3750/usr,system/usr)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/v3750/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/wiko/v3750/vendor,system/vendor)
